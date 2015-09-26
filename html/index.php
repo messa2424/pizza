ac06a4b134e5b0d596eca23ad16c24d69e92c937
messa2424 <micky.maik@gmail.com>
2015-09-26 03:25:05 +0200

    i have to stop

--- html/index.php
+++ html/index.php
@@ -1,5 +1,22 @@
 <DIV id="background">
     <Div id="main" class="box">
-        hello world
+        <?php
+        $DOM = simplexml_load_file('data/menu.xml');
+        
+        if( isset($DOM))
+        foreach ( $DOM->xpath('/menu/category') as $cat){
+            echo '<DIV class="category">'.$cat['name'].'</DIV>'."\n";
+            foreach ( $cat->item as $item ){
+                echo 
+                '<DIV class="item">'.
+                '<SPAN class="name">'.$item->name.'</SPAN>';
+                
+                foreach( $item->sizes->children()as $size) 
+                echo 
+                '<SPAN class="prize">'.$size.'</SPAN>';
+                echo '</DIV>'."\n";
+            }
+        }
+        ?>
     </Div>
 </DIV>
